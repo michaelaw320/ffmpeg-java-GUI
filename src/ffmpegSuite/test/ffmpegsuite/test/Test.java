5d299253eeb917e550f7732da6f6068d39d8c203
michaelaw320 <michaelaw320@gmail.com>
2016-08-18 01:10:58 +0700

    Proto Design

--- src/ffmpegSuite/test/ffmpegsuite/test/Test.java
+++ src/ffmpegSuite/test/ffmpegsuite/test/Test.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package test;
+package ffmpegsuite.test;
 
 /**
  *
@@ -16,6 +16,7 @@ public class Test {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Test command string");
     }
     
 }
